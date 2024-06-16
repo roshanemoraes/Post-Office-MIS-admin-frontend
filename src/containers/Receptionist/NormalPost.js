1bdc9a4753a87c4363b9d582794cd34b59400a84
Roshane Moraes <moraes.git@gmail.com>
2024-06-16 15:17:54 +0530

    Update a minor bug: feature/print receipt-to-pdf

--- src/containers/Receptionist/NormalPost.js
+++ src/containers/Receptionist/NormalPost.js
@@ -3,7 +3,6 @@ import React, { useRef } from "react";
 import PostForm from "../../components/Forms/PostForm";
 import { Navigate, useNavigate } from "react-router-dom";
 import { mailFormField } from "../../data/formFields";
-import { useReactToPrint } from "react-to-print";
 
 const NormalPost = () => {
   const Navigate = useNavigate();
@@ -18,44 +17,19 @@ const NormalPost = () => {
   const handleSubmit = async (formstate) => {
     console.log(formstate);
   };
-  const contentToPrint = useRef(null);
-  const handlePrint = useReactToPrint({
-    documentTitle: "TestPrint",
-    onBeforePrint: () => {
-      console.log("Before print");
-    },
-    onAfterPrint: () => {
-      console.log("After print");
-    },
-    removeAfterPrint: true,
-  });
 
   return (
-    <div ref={contentToPrint}>
-      <Box
-        display="flex"
-        flexDirection="row"
-        justifyContent="space-around"
-        p={2}
-      >
-        <PostForm
-          formTitle={"Normal Post"}
-          fieldsGroups={[
-            { label: "Recipient:", fields: recipientFields },
-            { label: "Transaction:", fields: transactionFields },
-          ]}
-          selectionGroups={[]}
-          onFormSubmit={handleSubmit}
-        />
-        <Button
-          onClick={() => {
-            handlePrint(null, () => contentToPrint.current);
-          }}
-        >
-          PRINT
-        </Button>
-      </Box>
-    </div>
+    <Box display="flex" flexDirection="row" justifyContent="space-around" p={2}>
+      <PostForm
+        formTitle={"Normal Post"}
+        fieldsGroups={[
+          { label: "Recipient:", fields: recipientFields },
+          { label: "Transaction:", fields: transactionFields },
+        ]}
+        selectionGroups={[]}
+        onFormSubmit={handleSubmit}
+      />
+    </Box>
   );
 };
 
