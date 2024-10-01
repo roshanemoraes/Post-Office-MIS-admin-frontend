4d083a013761f7fd051c4cb2f2ae332b77e2781b
Roshane Moraes <moraes.git@gmail.com>
2024-10-01 21:17:18 +0530

    Add PaymentFalling pages

--- src/containers/Customer/PaymentFailPage.jsx
+++ src/containers/Customer/PaymentFailPage.jsx
@@ -56,7 +56,7 @@ const PaymentFailPage = () => {
           <div>Error occured while processing the Money Order.</div>
           <div className="py-10 text-center">
             <Link
-              to="#"
+              to="/customer/money-order"
               className="px-12 bg-indigo-600 hover:bg-indigo-500 text-white font-semibold py-3"
             >
               GO BACK
--- src/containers/Customer/PaymentSuccessPage.jsx
+++ src/containers/Customer/PaymentSuccessPage.jsx
@@ -57,7 +57,7 @@ const PaymentSuccessPage = () => {
           <div>Money Order has successfully placed.</div>
           <div className="py-10 text-center">
             <Link
-              to="#"
+              to="/customer/money-order"
               className="px-12 bg-indigo-600 hover:bg-indigo-500 text-white font-semibold py-3"
             >
               GO BACK
