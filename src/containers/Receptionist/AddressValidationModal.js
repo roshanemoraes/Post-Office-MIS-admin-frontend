9b5b79f2d807bcd17067861da6ae9d009d5326a8
Roshane Moraes <moraes.git@gmail.com>
2024-10-05 10:09:03 +0530

    Enhance-NormalParcelPost

--- src/containers/Receptionist/AddressValidationModal.js
+++ src/containers/Receptionist/AddressValidationModal.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Button as MuiButton } from "@mui/material";
+import { Button as MuiButton, CircularProgress } from "@mui/material";
 import { Button, FormCheck, Modal, ModalHeader } from "react-bootstrap";
 import axios from "axios";
 
@@ -8,15 +8,30 @@ function AddressValidationModal({ formState, onValidationResult }) {
   const [validatedResponse, setValidatedResponse] = useState({});
   const [isValid, setIsValid] = useState(false);
   const [selectedCheckboxData, setSelectedCheckboxData] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const handleClose = () => {
     setShow(false);
     setValidatedResponse({});
     setIsValid(false);
     setSelectedCheckboxData(null);
+    setLoading(false);
   };
   const handleAccept = () => {
     onValidationResult(validatedResponse, selectedCheckboxData);
+    handleClose();
+    setSelectedCheckboxData(null);
+  };
+  const handleProceedWithoutValidation = () => {
+    const addressWithoutValidation = `${formState.recipientHouseNumber}, ${formState.recipientPostalZone}, ${formState.recipientCity}`;
+
+    const data = {
+      textForm: addressWithoutValidation,
+      addressId: null,
+      lat: null,
+      lng: null,
+    };
+    onValidationResult(data, null);
     handleClose();
     setSelectedCheckboxData(null);
   };
@@ -25,6 +40,7 @@ function AddressValidationModal({ formState, onValidationResult }) {
     setShow(true);
     setValidatedResponse({});
     setIsValid(false);
+    setLoading(true);
     const data = {
       houseNumber: formState.recipientHouseNumber,
       city: formState.recipientCity,
@@ -40,17 +56,17 @@ function AddressValidationModal({ formState, onValidationResult }) {
         console.log(response.data);
         setValidatedResponse(response.data);
         setIsValid(true);
+        setLoading(false);
         setShow(true);
       })
       .catch((err) => {
         console.log(err);
+        setLoading(false);
       });
   };
 
   const handleShow = () => {
     setShow(true);
-    console.log(formState.recipientHouseNumber);
-    console.log("Show");
   };
 
   const handleCheckboxChange = (customerId) => {
@@ -90,99 +106,119 @@ function AddressValidationModal({ formState, onValidationResult }) {
               alignItems: "center",
             }}
           >
-            <div style={{ color: "#0369a1" }}>Address Verification</div>
+            <div
+              style={{
+                color: "#000",
+                display: "flex",
+                justifyContent: "center",
+                justifySelf: "center",
+              }}
+            >
+              Address Verification
+            </div>
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          {isValid &&
+          {loading ? (
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                minHeight: "200px",
+              }}
+            >
+              <CircularProgress />
+            </div>
+          ) : isValid &&
             validatedResponse.members &&
-            Array.isArray(validatedResponse.members) && (
-              <div>
+            Array.isArray(validatedResponse.members) ? (
+            <div>
+              <div
+                style={{
+                  color: "#84cc16",
+                  fontSize: "20px",
+                  marginBottom: "0px",
+                  marginTop: "10px",
+                  fontWeight: "bold",
+                }}
+              >
+                Valid Address!
+              </div>
+              <br />
+              <div style={{ fontWeight: "bold" }}>Address: </div>
+              <div>{validatedResponse.textForm}</div>
+              <div style={{ marginTop: "12px" }}>
                 <div
-                  style={{
-                    color: "#84cc16",
-                    fontSize: "20px",
-                    marginBottom: "0px",
-                    marginTop: "10px",
-                    fontWeight: "bold",
-                  }}
+                  className="grid sm:grid-cols-12 xs:grid-cols-12"
+                  style={{ fontWeight: "bold", marginBottom: "1px" }}
                 >
-                  Valid Address!
-                </div>
-
-                <br />
-                <div style={{ fontWeight: "bold" }}>Address: </div>
-                <div>{validatedResponse.textForm}</div>
-                <div style={{ marginTop: "12px" }}>
+                  <div className="sm:col-span-9 xs:col-span-9">Members:</div>
                   <div
-                    className="grid sm:grid-cols-12 xs:grid-cols-12 "
-                    style={{ fontWeight: "bold", marginBottom: "1px" }}
+                    className="sm:col-span-2 xs:col-span-2"
+                    style={{
+                      justifyContent: "center",
+                    }}
                   >
-                    <div className="sm:col-span-9 xs:col-span-9">Members:</div>
-                    <div
-                      className="sm:col-span-2 xs:col-span-2"
-                      style={{
-                        justifyContent: "center",
-                      }}
-                    >
-                      Id
-                    </div>
-                    <div
-                      className="sm:col-span-1 xs:col-span-1"
-                      style={{
-                        display: "flex",
-                        alignSelf: "center",
-                        justifyContent: "left",
-                      }}
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="16"
-                        height="16"
-                        fill="currentColor"
-                        class="bi bi-bell-fill"
-                        viewBox="0 0 16 16"
-                      >
-                        <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2m.995-14.901a1 1 0 1 0-1.99 0A5 5 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901" />
-                      </svg>
-                    </div>
+                    Id
                   </div>
-                  {validatedResponse.members.map((member, index) => (
-                    <div key={index}>
-                      <div className="grid sm:grid-cols-12 xs:grid-cols-12">
-                        <div className="sm:col-span-9 xs:col-span-9">
-                          {member.name}
-                        </div>
-                        <div className="sm:col-span-2 xs:col-span-2">
-                          {member.customerId}
-                        </div>
-                        <div className="sm:col-span-1 xs:col-span-1">
-                          {member.customerId != "unreg" ? (
-                            <FormCheck
-                              type="checkbox"
-                              id={`checkbox-${index}`}
-                              label=""
-                              checked={
-                                selectedCheckboxData === member.customerId
-                              }
-                              onChange={() =>
-                                handleCheckboxChange(member.customerId)
-                              }
-                            />
-                          ) : null}
-                        </div>
+                  <div
+                    className="sm:col-span-1 xs:col-span-1"
+                    style={{
+                      display: "flex",
+                      alignSelf: "center",
+                      justifyContent: "left",
+                    }}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      className="bi bi-bell-fill"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2m.995-14.901a1 1 0 1 0-1.99 0A5 5 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901" />
+                    </svg>
+                  </div>
+                </div>
+                {validatedResponse.members.map((member, index) => (
+                  <div key={index}>
+                    <div className="grid sm:grid-cols-12 xs:grid-cols-12">
+                      <div className="sm:col-span-9 xs:col-span-9">
+                        {member.name}
+                      </div>
+                      <div className="sm:col-span-2 xs:col-span-2">
+                        {member.customerId}
+                      </div>
+                      <div className="sm:col-span-1 xs:col-span-1">
+                        {member.customerId !== "unreg" ? (
+                          <FormCheck
+                            type="checkbox"
+                            id={`checkbox-${index}`}
+                            label=""
+                            checked={selectedCheckboxData === member.customerId}
+                            onChange={() =>
+                              handleCheckboxChange(member.customerId)
+                            }
+                          />
+                        ) : null}
                       </div>
                     </div>
-                  ))}
-                </div>
-                <div
-                  style={{ marginTop: "30px", color: "blue", fontSize: "14px" }}
-                >
-                  Press 'Accept' to use this Address, 'Reject' to discard.
-                </div>
+                  </div>
+                ))}
               </div>
-            )}
-          {!isValid && (
+              <div
+                style={{
+                  marginTop: "30px",
+                  color: "blue",
+                  fontSize: "14px",
+                }}
+              >
+                Press 'Accept' to use this Address, 'Reject' to discard.
+              </div>
+            </div>
+          ) : (
             <div>
               <div
                 style={{
@@ -196,7 +232,11 @@ function AddressValidationModal({ formState, onValidationResult }) {
                 Invalid Address !!
               </div>
               <div
-                style={{ marginTop: "10px", fontSize: "14px", color: "blue" }}
+                style={{
+                  marginTop: "10px",
+                  fontSize: "14px",
+                  color: "blue",
+                }}
               >
                 Please Check Again.
               </div>
@@ -204,12 +244,22 @@ function AddressValidationModal({ formState, onValidationResult }) {
           )}
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Reject
-          </Button>
-          <Button disabled={!isValid} variant="primary" onClick={handleAccept}>
-            Accept
-          </Button>
+          {isValid ? (
+            <Button
+              disabled={!isValid}
+              variant="primary"
+              onClick={handleAccept}
+            >
+              Proceed
+            </Button>
+          ) : (
+            <Button
+              variant="secondary"
+              onClick={handleProceedWithoutValidation}
+            >
+              Proceed Without Validation
+            </Button>
+          )}
         </Modal.Footer>
       </Modal>
     </>
---
+++ src/containers/Receptionist/MailRegistration/CostFormNew.jsx
@@ -0,0 +1,144 @@
+import {
+  Box,
+  useTheme,
+  Typography,
+  TextField,
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Menu,
+  Select,
+} from "@mui/material";
+
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+const CostFormNew = ({ postType, description, onCostUpdate }) => {
+  const theme = useTheme();
+  const [weight, setWeight] = useState(null);
+  const [cost, setCost] = useState(null);
+  const [selectedOption, setSelectedOption] = useState("");
+  const [type, setType] = React.useState("");
+
+  const handleChange = (event) => {
+    setWeight(event.target.value);
+  };
+  const calculateCost = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:8081/postage/get/${postType}?weight=${weight}`,
+        { withCredentials: true }
+      );
+      console.log("response came:", response.data.price);
+      setCost(response.data.price);
+      onCostUpdate(response.data.price);
+    } catch (error) {
+      console.error("Error fetching postage", error);
+    }
+  };
+
+  return (
+    <Box
+      p={2}
+      sx={{
+        display: "flex",
+        // flexDirection: "row",
+        // alignItems: "center",
+        // justifyContent: "space-around",
+        marginTop: "16px",
+        // marginLeft: "16px",
+        // width: "260px",
+        // height: "230px",
+        backgroundColor: "#fff",
+        borderRadius: "10px",
+        padding: "0 0 5px 0",
+        // boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.1)",
+      }}
+    >
+      {/* <Typography
+        variant="subtitle2"
+        sx={{
+          marginTop: "20px",
+          marginBottom: "10px",
+          fontSize: "22px",
+          marginBottom: "10px",
+          fontFamily: "Helvetica Neue",
+          //   color: theme.palette.text.typography,
+          fontWeight: "bold",
+        }}
+      >
+        Postage Finder
+      </Typography> */}
+
+      <Box
+        component={"form"}
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          //   "& .MuiTextField-root": { m: 1, minWidth: 200 },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <div>
+          <div
+            style={{
+              fontSize: "14px",
+              marginBottom: "10px",
+              marginTop: "10px",
+            }}
+          >
+            {description}
+          </div>
+          <TextField
+            required
+            id="outlined-required"
+            label="Weight (grams)"
+            style={{
+              backgroundColor: theme.palette.background.inputField,
+              minWidth: 150,
+            }}
+            value={weight}
+            onChange={handleChange}
+            inputProps={{ style: { fontSize: 15 } }}
+            InputLabelProps={{
+              style: { fontSize: 13, width: "500px" },
+            }}
+          />
+        </div>
+        <Button
+          //   variant="contained"
+          sx={{
+            backgroundColor: "#d1d5db",
+            color: "#000",
+            mt: 1,
+            px: 1,
+            mb: 2,
+            fontSize: "12px",
+            border: "2px solid #d1d5db",
+            "&:hover": {
+              backgroundColor: "#b0b3b8",
+            },
+          }}
+          onClick={calculateCost}
+        >
+          Get Postage
+        </Button>
+        {cost && (
+          <div style={{ marginTop: "10px", marginBottom: "20px" }}>
+            Postage: {cost !== null ? `Rs.${cost}.00` : ""}
+          </div>
+        )}
+      </Box>
+    </Box>
+  );
+};
+
+export default CostFormNew;
