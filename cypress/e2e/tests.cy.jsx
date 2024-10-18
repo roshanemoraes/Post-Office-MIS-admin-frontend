bec2e867e1054a82f05f50b31ba0ae9b8d6f804f
Kokila Meddawitage <meddawitagekcd.21@uom.lk>
2024-10-18 15:20:01 +0530

    Add Login e2e

--- cypress/e2e/tests.cy.jsx
+++ cypress/e2e/tests.cy.jsx
@@ -1,5 +1,18 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+/*describe("Login Functionality", () => {
+  it("Login", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get(":nth-child(1) > .MuiButtonBase-root").click();
+    cy.get('[data-cy="email-input"]').type("postmaster@gmail.com");
+    cy.get('[data-cy="password-input"]').type("123");
+    cy.get('[data-cy="login-button"]').click();
+  });
+});*/
+describe("Login Functionality", () => {
+  it("Login", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get(":nth-child(1) > .MuiButtonBase-root").click();
+    cy.get("#\\:r1\\:").type("postmaster@gmail.com");
+    cy.get("#\\:r3\\:").type("123");
+    cy.get(":nth-child(5) > .MuiButtonBase-root").click();
+  });
+});
